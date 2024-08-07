5adfcc9d4a0c9e5dfa34fce5e98c5646530699ca
oditynet <oditynet@gmail.com>
2024-08-07 12:31:30 +0300

    Update krb5client.cpp

--- krb5client.cpp
+++ krb5client.cpp
@@ -28,7 +28,21 @@
 #include <pwd.h>
 using namespace std;
 int sockfd, curs_start, use_curses, debug_flag;
-
+static inline int data_eq(krb5_data d1, krb5_data d2)
+{
+    return (d1.length == d2.length && (d1.length == 0 ||
+                                       !memcmp(d1.data, d2.data, d1.length)));
+}
+static krb5_boolean is_local_tgt(krb5_principal princ, krb5_data *realm)
+{
+    return princ->length == 2 && data_eq(princ->realm, *realm); //&&
+       // data_eq_string(princ->data[0], KRB5_TGS_NAME) &&
+       // data_eq(princ->data[1], *realm);
+}
+bool ts_after(krb5_timestamp a, krb5_timestamp b)
+{
+    return (uint32_t)a > (uint32_t)b;
+}
 int main(int argc, char **argv) {
 	char *execstr = NULL;
 	int ret, writebufflen;
@@ -79,7 +93,25 @@ int main(int argc, char **argv) {
 		cout<<"Error: krb5_cc_get_principal";
 		exit(1);
 	}
-	ret = krb5_unparse_name(context, my_principal, &my_principal_string);
+	krb5_cc_cursor cur = NULL;
+	 krb5_creds creds;
+	 krb5_timestamp now;
+
+	 now = time(0);
+	 ret=krb5_cc_start_seq_get(context, ccache, &cur);
+	 while ((ret = krb5_cc_next_cred(context, ccache, &cur, &creds)) == 0) {
+	         if (is_local_tgt(creds.server, &my_principal->realm)) {
+	             if (ts_after(creds.times.endtime, now))
+	            	 cout<<"Ticket is valid"<<endl;
+	             else
+	             {
+	            	 cout<<"Ticket is expired";
+	            	 exit(1);
+	             }
+	         }
+	         krb5_free_cred_contents(context, &creds);
+	     }
+	 ret = krb5_unparse_name(context, my_principal, &my_principal_string);
 	if (ret){
 		cout<<"Error: krb5_unparse_name";
 		exit(1);
